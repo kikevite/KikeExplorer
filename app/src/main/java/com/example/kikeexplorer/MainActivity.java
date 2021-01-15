23e81a0d2c02210d3b5bc39bb0e82c94f544fced
3MI <brianbvf@gmail.com>
2021-01-15 19:37:41 +0100

    no funciona... nose pk

--- app/src/main/java/com/example/kikeexplorer/MainActivity.java
+++ app/src/main/java/com/example/kikeexplorer/MainActivity.java
@@ -1,14 +1,141 @@
 package com.example.kikeexplorer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileOutputStream;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 0;
+    private static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 1;
+
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text = findViewById(R.id.text);
+
+
+
+        // si no tens permis, demanar permis
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            // Si tens permis per escriure...
+            getReadPermise();
+
+        } else {
+            // Si no tens permis per escriure...
+            getWritePermise();
+        }
+
+
+        // coses a fer
+        String rutaInicial = Environment.getExternalStorageDirectory().toString();
+        String carpetaActual = "/aaaKike";
+
+        String noms = "";
+        noms += getCarpetes(rutaInicial + carpetaActual);
+        text.setText(noms);
+
+    }
+
+    private String getCarpetes(String path) {
+        String llista = "";
+
+        Log.d("Files", "Path: " + path);
+        File directory = new File(path);
+        File[] files = directory.listFiles();
+        Log.d("Files", "Size: "+ files.length);
+        for (int i = 0; i < files.length; i++)
+        {
+            Log.d("Files", "FileName:" + files[i].getName());
+            llista += files[i] + "\n";
+        }
+        return llista;
+    }
+
+    private void getWritePermise() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
+        } else {
+            // coses a fer, pk ja tenim el permis
+            //...
+        }
+    }
+
+    private void getReadPermise() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+        } else {
+            // coses a fer, pk ja tenim el permis
+            //...
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Log.i("kike", "permis lectura concedit");
+                } else {
+                    //Log.i("kike", "permis lectura denegat");
+                }
+                return;
+            }
+            case MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Log.i("kike", "permis escriptura concedit");
+
+                } else {
+                    //Log.i("kike", "permis escriptura denegat");
+                }
+            }
+        }
+    }
+
+
+    public void writeFileExternalStorage(String filenameExternal) {
+
+        //Text of the Document
+        String textToWrite = "bla bla bla";
+
+        //Checking the availability state of the External Storage.
+        String state = Environment.getExternalStorageState();
+        if (!Environment.MEDIA_MOUNTED.equals(state)) {
+
+            //If it isn't mounted - we can't write into it.
+            return;
+        }
+
+        //Create a new file that points to the root directory, with the given name:
+        File file = new File(getExternalFilesDir(null), filenameExternal);
+
+        //This point and below is responsible for the write operation
+        FileOutputStream outputStream = null;
+        try {
+            file.createNewFile();
+            //second argument of FileOutputStream constructor indicates whether
+            //to append or create new file if one exists
+            outputStream = new FileOutputStream(file, true);
+
+            outputStream.write(textToWrite.getBytes());
+            outputStream.flush();
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
